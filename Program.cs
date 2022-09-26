b7c66dcc426ab0a4323bb5c76733fb02db3176b9
Korisnik <Korisnik@DESKTOP-Q60U0Q5>
2022-09-26 12:09:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje4
+{
+    class Biljka
+    {
+
+    }
+    class Stablo:Biljka
+    {
+        bool opadajuListovi;
+
+        public Stablo(bool opadajuListovi)
+        {
+            this.opadajuListovi = opadajuListovi;
+        }
+
+        public bool OpadajuListovi { get => opadajuListovi; set => opadajuListovi = value; }
+
+    }
+    class Cvijet:Biljka
+    {
+
+    }
+    class Bijelogoricno : Stablo
+    {
+        public Bijelogoricno(bool opadajuListovi) : base(opadajuListovi)
+        {
+        }
+    }
+    class Crnogoricno : Stablo
+    {
+        public Crnogoricno(bool opadajuListovi) : base(opadajuListovi)
+        {
+        }
+    }
+
+
+    /*
+     * Definirajte klasu Biljka, te definirajte od nje izvedenu klasu Stablo sa sljedećim članicama:
+• varijabla tipa bool opadajuListovi
+• svojstvo OpadajuListovi
+• konstruktor Stablo(bool) koji će dodijeliti vrijednost proslijeđenog argumenta varijabli
+opadajuListovi
+Definirajte klasu Cvijet izvedenu iz klase Biljka.
+Definirajte klase Bijelogoricno i Crnogoricno izvedene iz klase Stablo.
+Klasama definirajte odgovarajuće konstruktore.
+Definirajte dva objekta Hrast i Bor tipova Bijelogoricno i Crnogoricno, te ispišite vrijednosti
+njihovih članica opadajuListovi.
+
+     * */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bijelogoricno Hrast = new Bijelogoricno(true);
+
+            Crnogoricno Bor = new Crnogoricno(false);
+            Console.WriteLine(Hrast.OpadajuListovi);
+            Console.WriteLine(Bor.OpadajuListovi);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ponavljanje4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ponavljanje4")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5882e456-96a8-447c-aa4c-d64e8e1b4fa2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
